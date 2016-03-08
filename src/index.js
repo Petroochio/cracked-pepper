3eb296ce494cb87091ce374b3495a5633597f586
Peter Gyory <ptg3662@rit.edu>
2016-03-08 09:46:44 -0500

    Add key vars to pepper effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-// entry point file
+// lib
+import R from 'ramda';
+import { PerspectiveCamera, Vector3, Quaternion } from 'three';
+
+/**
+ * Constructor for peppers ghost effect renderer
+ * @param {THREE.WebGLRenderer} renderer
+ */
+const pepper = renderer => {
+  // Public vars
+  this.pos = new Vector3(); // Position
+  this.quat = new Quaternion(); // Quaternion
+  this.scale = new Vector3(); // Scale :P
+  this.viewDistance = 10;
+
+  this.renderer = renderer;
+  // Allow for rendering multiple times in single frame
+  this.renderer.autoClear = false;
+
+  // Private vars
+  // Cameras
+  const cameras = {
+    front: new PerspectiveCamera(),
+    back: new PerspectiveCamera(),
+    left: new PerspectiveCamera(),
+    right: new PerspectiveCamera(),
+  };
+
+  // Sizing
+  let halfWidth;
+  let height;
+  let width;
+
+  this.setSize = (width, height) => {
+
+  };
+
+  this.render = () => {
+
+  };
+};
+
+export default pepper;
