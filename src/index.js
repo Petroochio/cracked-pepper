3d35b0ccaa86236e92e8f7ef2fc791746139bbc9
Peter Gyory <ptg3662@rit.edu>
2016-03-08 15:06:10 -0500

    Add camera update logic

--- src/index.js
+++ src/index.js
@@ -19,18 +19,24 @@ const pepper = renderer => {
 
   // Private vars
   // Cameras
-  const cameras = {
-    front: new PerspectiveCamera(),
-    back: new PerspectiveCamera(),
-    left: new PerspectiveCamera(),
-    right: new PerspectiveCamera(),
-  };
+  const cameras = [
+    new PerspectiveCamera(),
+    new PerspectiveCamera(),
+    new PerspectiveCamera(),
+    new PerspectiveCamera(),
+  ];
 
   // Sizing
   let halfWidth;
   let height;
   let width;
 
+  /**
+   * Sets effect height and widht
+   * @param {int} w : width of viewport
+   * @param {int} h : height of viewport
+   * @return this
+   */
   this.setSize = (w, h) => {
     renderer.setSize(w, h);
     halfWidth = w / 2;
@@ -39,7 +45,48 @@ const pepper = renderer => {
     console.log('w:', width, 'h:', height, 'w/2:', halfWidth);
   };
 
-  this.render = () => {
+  /**
+   * Sets effect properties from camera object
+   * @param {THREE.PerspectiveCamera} camera
+   * @return this
+   */
+  this.setFromCamera = camera => {
+    if (R.isNil(camera.parent)) camera.updateMatrixWorld();
+    else camera.parent.updateMatrixWorld();
+
+    // apply camera transforms to pepper effect
+    camera.matrixWorld.decompose(this.pos, this.quat, this.scale);
+  };
+
+  const updateCameras = center => {
+    R.forEach(camera => {
+      camera.position.copy(this.pos);
+      camera.quaternion.copy(this.quat);
+    }, cameras);
+
+    const [front, back, left, right] = cameras;
+
+    front.translateZ(this.viewDistance);
+    front.lookAt(center);
+
+    back.translateZ(-(this.viewDistance));
+    back.lookAt(center);
+    back.rotation.z += Math.PI;
+
+    left.translateX(-(this.viewDistance));
+    left.lookAt(center);
+    left.rotation.x += Math.PI / 2;
+
+    right.translateX(this.viewDistance);
+    right.lookAt(center);
+    right.rotation.x += Math.PI / 2;
+  };
+
+  this.render = scene => {
+    updateCameras(scene.position);
+
+    this.renderer.clear();
+    this.renderer.setScissorTest(true);
 
   };
 };
